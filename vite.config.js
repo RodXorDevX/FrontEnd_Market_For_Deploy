031921d18eaac4f44551f64f03117d952c4698fe
RodXorDevX <rodxordevx@gmail.com>
2025-04-10 16:39:02 -0400

    Update Vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Asegura que el base URL sea "/" para la aplicación
 })
